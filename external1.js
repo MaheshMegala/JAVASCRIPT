a23b7e92eb029f740c693b4a5eece96b19a31863
MaheshMegala <maheshmegala7@gmail.com>
2025-03-13 14:02:14 +0530

    Delete external1.js

--- external1.js
+++
@@ -1,9 +0,0 @@
-document.write('<h2>welcome to my world</h2>');
-document.write('<p>This website is related to Full stack developers</p>');
-document.write('<p>Full stack developers are those who have knowledge of both front end and back end development</p>');
-document.write('<p>They are the most demanded developers in the market</p>');
-document.write('<p>They are the most paid developers in the market</p>');
-document.write('<p>They are the most respected developers in the market</p>');
-document.write('<p>They are the most happiest developers in the market</p>');
-document.write('<p>They are the most successful developers in the market</p>');
-
